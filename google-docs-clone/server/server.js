6e9a3af48402f8498d819f182c6430afde8738c0
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-22 22:57:47 -0500

    google-doc-clone with Socket.io

---
+++ google-docs-clone/server/server.js
@@ -0,0 +1,39 @@
+require("./config/mongoose.config");
+const Document = require("./models/documents.model");
+
+const { default: mongoose } = require("mongoose");
+const { Socket } = require("socket.io");
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+// Socket io
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+  console.log("connected to socket");
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}

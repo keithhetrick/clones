b68898b80d74ec67a8b419ca679f49483c00a35b
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-30 22:44:14 -0500

    disney+ clone - start

---
+++ disney-plus-clone/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
